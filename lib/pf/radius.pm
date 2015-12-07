5f5a01c8f6f099b6b97684a6cb506138fdf35d76
James Rouzier <jrouzier@inverse.ca>
2015-12-07 11:50:29 -0500

    Add the radius audit information to the return

--- lib/pf/radius.pm
+++ lib/pf/radius.pm
@@ -81,7 +81,7 @@ sub authorize {
     my $logger = $self->logger;
     my $start = Time::HiRes::gettimeofday();
     my($switch_mac, $switch_ip,$source_ip,$stripped_user_name,$realm) = $self->_parseRequest($radius_request);
-
+    my $RAD_REPLY_REF;
 
     $logger->debug("instantiating switch");
     my $switch = pf::SwitchFactory->instantiate({ switch_mac => $switch_mac, switch_ip => $switch_ip, controllerIp => $source_ip});
@@ -92,9 +92,10 @@ sub authorize {
             "Can't instantiate switch ($switch_ip). This request will be failed. "
             ."Are you sure your switches.conf is correct?"
         );
-        $pf::StatsD::statsd->end(called() . ".timing" , $start);
-        return [ $RADIUS::RLM_MODULE_FAIL, ('Reply-Message' => "Switch is not managed by PacketFence") ];
+        $RAD_REPLY_REF = [ $RADIUS::RLM_MODULE_FAIL, ('Reply-Message' => "Switch is not managed by PacketFence") ];
+        goto AUDIT;
     }
+
 
     my ($nas_port_type, $eap_type, $mac, $port, $user_name, $nas_port_id, $session_id) = $switch->parseRequest($radius_request);
     Log::Log4perl::MDC->put( 'mac', $mac );
@@ -144,8 +145,8 @@ sub authorize {
     my $weActOnThisCall = $self->_doWeActOnThisCall($args);
     if ($weActOnThisCall == 0) {
         $logger->info("We decided not to act on this radius call. Stop handling request from $switch_ip.");
-        $pf::StatsD::statsd->end(called() . ".timing" , $start);
-        return [ $RADIUS::RLM_MODULE_NOOP, ('Reply-Message' => "Not acting on this request") ];
+        $RAD_REPLY_REF = [ $RADIUS::RLM_MODULE_NOOP, ('Reply-Message' => "Not acting on this request") ];
+        goto CLEANUP;
     }
 
     $logger->info("handling radius autz request: from switch_ip => ($switch_ip), "
@@ -174,8 +175,8 @@ sub authorize {
     # verify if switch supports this connection type
     if (!$self->_isSwitchSupported($args)) {
         # if not supported, return
-        $pf::StatsD::statsd->end(called() . ".timing" , $start);
-        return $self->_switchUnsupportedReply($args);
+        $RAD_REPLY_REF = $self->_switchUnsupportedReply($args);
+        goto CLEANUP;
     }
 
 
@@ -215,17 +216,16 @@ sub authorize {
     # if it's an IP Phone, let _authorizeVoip decide (extension point)
     if ($isPhone) {
         $pf::StatsD::statsd->end(called() . ".timing" , $start);
-        return $self->_authorizeVoip($args);
+        $RAD_REPLY_REF = $self->_authorizeVoip($args);
+        goto CLEANUP;
     }
 
     # if switch is not in production, we don't interfere with it: we log and we return OK
     if (!$switch->isProductionMode()) {
         $logger->warn("Should perform access control on switch ($switch_id) but the switch "
             ."is not in production -> Returning ACCEPT");
-        $switch->disconnectRead();
-        $switch->disconnectWrite();
-        $pf::StatsD::statsd->end(called() . ".timing" , $start);
-        return [ $RADIUS::RLM_MODULE_OK, ('Reply-Message' => "Switch is not in production, so we allow this request") ];
+        $RAD_REPLY_REF = [ $RADIUS::RLM_MODULE_OK, ('Reply-Message' => "Switch is not in production, so we allow this request") ];
+        goto CLEANUP;
     }
 
     # Check if a floating just plugged in
@@ -257,10 +257,15 @@ sub authorize {
     }
 
     my $RAD_REPLY_REF = $switch->returnRadiusAccessAccept($args);
+
+CLEANUP:
     # cleanup
     $switch->disconnectRead();
     $switch->disconnectWrite();
 
+AUDIT:
+
+    push @$RAD_REPLY_REF, $self->_addRadiusAudit;
     $pf::StatsD::statsd->end(called() . ".timing" , $start, 0.25 );
 
     return $RAD_REPLY_REF;
@@ -599,7 +604,7 @@ sub _handleStaticPortSecurityMovement {
 
     my $old_switch_id = $locationlog_mac->{'switch'};
     #Nothing to do if it is the same switch
-    if ( $old_switch_id eq $args->{'switch'}->{_id} ) { 
+    if ( $old_switch_id eq $args->{'switch'}->{_id} ) {
         $pf::StatsD::statsd->end(called() . ".timing" , $start, 0.25 );
         return undef;
     }
