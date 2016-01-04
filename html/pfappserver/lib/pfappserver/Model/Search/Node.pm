ccb9c590e899977d779dc51cacb990954c614877
James Rouzier <jrouzier@inverse.ca>
2016-01-04 11:44:58 -0500

    Added the ability to search by violation status

--- html/pfappserver/lib/pfappserver/Model/Search/Node.pm
+++ html/pfappserver/lib/pfappserver/Model/Search/Node.pm
@@ -275,6 +275,30 @@ my %COLUMN_MAP = (
             }
         ]
     },
+    violation_status   => {
+        table => 'violation',
+        name  => 'status',
+        joins => [
+            {
+                'table'  => 'violation',
+                'join' => 'LEFT',
+                'on' =>
+                [
+                    [
+                        {
+                            'table' => 'violation',
+                            'name'  => 'mac',
+                        },
+                        '=',
+                        {
+                            'table' => 'node',
+                            'name'  => 'mac',
+                        }
+                    ],
+                ],
+            },
+        ]
+    },
 );
 
 sub add_order_by {
